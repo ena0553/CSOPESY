413bb333641a20f9cafc1734843bdfc06d112128
JustinChing30 <justin_ethan_l_ching@dlsu.edu.ph>
2026-06-17 18:57:35 +0800

    Added Process class

---
+++ Process.cpp
@@ -0,0 +1,18 @@
+#include "Process.h"
+
+// brace initialization for constructor
+Process::Process(int pid, const std::string& type, const std::string& name, const std::string& memoryUsage)
+	: pid{ pid }, type{ type }, name{ name }, memoryUsage{ memoryUsage }, currentState{ READY } {
+}
+
+void Process::setProcessState(ProcessState state)
+{
+	currentState = state;
+}
+
+// getters
+int Process::getPID() const { return pid; } // process ID
+std::string Process::getType() const { return type; } // process type
+std::string Process::getName() const { return name; } // process name
+std::string Process::getMemoryUsage() const { return memoryUsage; } // GPU memory usage
+Process::ProcessState Process::getState() const { return currentState; } // process state from enums
---
+++ Process.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <string>
+
+class Process
+{
+public:
+	Process(int pid, const std::string& type, const std::string& name, const std::string& memoryUsage); // constructor
+
+	enum ProcessState // process's state
+	{
+		READY,
+		RUNNING,
+		FINISHED
+	};
+
+	void setProcessState(ProcessState state); // set process state
+
+	// getters
+	int getPID() const; // process ID
+	std::string getType() const;  // process type
+	std::string getName() const; // process name
+	std::string getMemoryUsage() const;  // GPU memory usage
+	ProcessState getState() const; // process state from enums
+
+private:
+	int pid; // process ID
+	std::string type; // process type
+	std::string name; // process name
+
+	int cpuCoreID = -1; // CPU core ID that process is running on, -1 if not running(?)
+	std::string memoryUsage; // GPU memory usage
+
+	ProcessState currentState; // process's state
+	int totalCommands = 100; /* 100 print commands for Week 6 - Group Homework - FCFS scheduler in OS emulator
+	in our final program this will be the list of ICommands */
+	int commandCounter = 0; // index of the current command being executed
+};
