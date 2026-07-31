24b2ed35d4dbf9ccfe0c8a26bacca0e4245561e3
cc0553 <enriquemiguelaragon@gmail.com>
2026-07-31 23:12:14 +0800

    fix: typo in main

--- main.cpp
+++ main.cpp
@@ -526,7 +526,7 @@ int main() {
         if (initialize(config)) {
             // create the memory manager
             int memPerProc = getRandomPowerOfTwo(config.minMemPerProc, config.maxMemPerProc);
-			memManager = make_unique<MemoryManager>(config.maxOverallMem, config.memPerFramem);
+			memManager = make_unique<MemoryManager>(config.maxOverallMem, config.memPerFrame);
 
             // create the scheduler
             scheduler = make_unique<ProcessScheduler>(config.numCpu, config.scheduler, config.quantumCycles, config.delayPerExec, memManager.get());
